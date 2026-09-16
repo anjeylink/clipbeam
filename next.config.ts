cfd7da92ca015fa8f39de81cac45de5c826c031f
andrii <anjey.link@gmail.com>
2026-09-16 15:19:39 +0300

    trying to center text

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import { withIntlayer } from "next-intlayer/server";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["ubuntu"],
 };
 
 export default withIntlayer(nextConfig);
